c8ee986e234d6fad0069eb67cabc4bb878f2cf5d
RomelAnte <rsag20092002@gmail.com>
2025-02-02 16:35:56 -0500

    Update app names to include the 'Aplicaciones' namespace for consistency

--- Aplicaciones/comunicados/apps.py
+++ Aplicaciones/comunicados/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ComunicadosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'comunicados'
+    name = 'Aplicaciones.comunicados'
--- Aplicaciones/medidores/apps.py
+++ Aplicaciones/medidores/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MedidoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medidores'
+    name = 'Aplicaciones.medidores'
