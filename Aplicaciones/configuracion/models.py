1c10654fd88009f2713e49a683575b9b5a043730
RomelAnte <rsag20092002@gmail.com>
2025-02-01 22:42:10 -0500

    Add models for asistencia, comunicados, configuracion, and consumo; update database settings for MySQL

--- Aplicaciones/configuracion/models.py
+++ Aplicaciones/configuracion/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Configuracion(models.Model):
+    id_con 	= models.AutoField(primary_key = True)
+    nombre_con 	= models.CharField(max_length = 250)
+    ruc_con = models.CharField(max_length = 13)	
+    logo_con = models.CharField(max_length = 200)	
+    telefono_con = models.CharField(max_length = 10) 	
+    direccion_con = models.CharField(max_length = 50)	
+    email_con = models.CharField(max_length = 50)	
+    servidor_con = models.CharField(max_length = 50)
+    puerto_con = models.CharField(max_length = 50)	
+    password_con = models.CharField(max_length=50)	
+    creacion_con = models.DateTimeField()	
+    actualizacion_con = models.DateTimeField()	
+    anio_inicial_con = models.IntegerField()	
+    mes_inicial_con = models.CharField(max_length=25)
--- Aplicaciones/facturacion/models.py
+++ Aplicaciones/facturacion/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Consumo (models.Model):
+    id_consumo = models.AutoField(primary_key=True)
+    anio_consumo = models.IntegerField()	
+    mes_consumo = models.CharField(max_length = 20)	
+    estado_consumo = models.CharField(max_length = 20)	
+    fecha_creacion_consumo = models.DateTimeField()	
+    fecha_actualizacion_consumo = models.DateTimeField()	
+    numero_mes_consumo = models.IntegerField()	
+    fecha_vencimiento_consumo = models.DateField()
+    
