a0fd466f94f63dd81f3d52a0c5b87b9cee7270f5
RomelAnte <rsag20092002@gmail.com>
2025-02-06 22:56:54 -0500

    Add total water consumption chart and utility function; update dashboards template and view context

--- Aplicaciones/home/views.py
+++ Aplicaciones/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .utils import promedio_consumo
+from .utils import promedio_consumo, estado_lectura, total_consumo_mensual
 
 # Create your views here.
 def home(request):
@@ -8,5 +8,7 @@ def home(request):
 def dashboards(request):
     context = {
         'consumo_mensual': promedio_consumo(),
+        'estado_lectura': estado_lectura(),
+        'total_consumos': total_consumo_mensual()
     }
     return render(request, 'dashboards.html', context)
