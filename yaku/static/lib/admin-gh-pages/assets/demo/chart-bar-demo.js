a06b6c85dcc1498b27a5b3e3eb10cdad41fa5762
RomelAnte <rsag20092002@gmail.com>
2025-02-07 01:12:28 -0500

    Remove default font settings from chart demo scripts; add pie chart demo script to templates

--- yaku/static/lib/admin-gh-pages/assets/demo/chart-bar-demo.js
+++ yaku/static/lib/admin-gh-pages/assets/demo/chart-bar-demo.js
@@ -1,3 +1,5 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
@@ -9,6 +11,7 @@ var myLineChart = new Chart(ctx, {
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
       data: [4215, 5312, 6251, 7841, 9821, 14984],
+
     }],
   },
   options: {
