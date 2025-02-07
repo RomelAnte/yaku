7952a4a2be02a97dcb4fec954869af0974061a08
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2025-02-06 21:59:26 -0500

    Add dashboards view and template; implement promedio_consumo utility function and SQL procedure

---
+++ Aplicaciones/home/utils.py
@@ -0,0 +1,37 @@
+from django.db import connection
+from decimal import Decimal
+
+def promedio_consumo():
+    with connection.cursor() as cursor:
+        cursor.callproc('promedio_consumo_mensual')
+        result = cursor.fetchall()
+    
+    # Asegúrate de que result no esté vacío
+    if not result:
+        return []
+
+    return [
+        {
+            'mes': row[0],  # 'JULIO'
+            'anio': row[1],  # 2023
+            'promedio': row[2]  # Decimal('-19.738007') convertido a float
+        }
+        for row in result
+    ]
+    
+def estado_lectura():
+    with connection.cursor() as cursor:
+        cursor.callproc('estado_lectura')
+        result = cursor.fetchall()
+    
+    # Asegúrate de que result no esté vacío
+    if not result:
+        return []
+
+    return [
+        {
+            'estado_lec': row[0],  # 'JULIO'
+            'cantidad': row[1],  # 2023
+        }
+        for row in result
+    ]
